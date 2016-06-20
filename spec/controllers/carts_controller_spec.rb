de0b7498ffd008be23f06da30e19b06d118d2def
stephen_leez@sina.com <stephen@stephendeMac.local>
2016-06-20 20:10:37 +0800

    补充

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CartsController, type: :controller do
-
+  #具体测试在users,products,orders
 
 
 end
--- spec/models/authority_spec.rb
+++ spec/models/authority_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Authority, type: :model do
-  
+  #具体测试在user,order,mark,coupon,cart
 end
